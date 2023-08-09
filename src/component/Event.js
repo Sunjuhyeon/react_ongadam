823a026789ec21e54c0166c4022074579e044da5
dongsam-E <dong2dong32@gmail.com>
2023-08-09 18:03:36 +0900

    이벤트섹션 디자인 뼈대 잡음
    1. 사진 backgroundImage로 설정
    2. 반응형 미적용

--- src/component/Event.js
+++ src/component/Event.js
@@ -5,13 +5,29 @@ function Event(props) {
   return (
     <section id={props.id} className={`${e.section} py-5`}>
         <ul className={`d-lg-flex`}>
-            <li className={`col-lg-6`}>
-              <div  className={`event1`}><a href=""></a></div>
+            <li>
+              <div  className={`event0 ${e.eventlist}`}>
+                <a href=""></a>
+              </div>
             </li>
-            <li className={`col-lg-6`}>
-              <div i className={`event0`}><a href=""></a></div>
-              <div i className={`event2`}><a href=""></a></div>
-            </li>
+            <div>
+              <li className={`d-lg-flex align-items-center`}>
+                <div i className={`event1 ${e.eventlist} ${e.smbox}`}>
+                  <a href=""></a>
+                </div>
+                <div className={`${e.textbox}`}>
+                  <p className={`mb-0`}>캠핑에 미치다 X 온가담</p>
+                  <span>최대 30%세일 오픈!!</span>
+                  <a href="">바로가기 <img src="/img/arrow.png" alt="arrow" /></a>
+                </div>
+              </li>
+              <li className={`d-lg-flex align-items-center`}>
+                <div className={`${e.textbox}`}>설명입니다.</div>
+                <div i className={`event2 ${e.eventlist} ${e.smbox}`}>
+                  <a href=""></a>
+                </div>
+              </li>
+            </div>
         </ul>
     </section>
   )
