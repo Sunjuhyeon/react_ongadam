933c329697788b32ddc81187f07bcc033af8faf5
dongsam-E <dong2dong32@gmail.com>
2023-08-10 11:52:05 +0900

    1차 반응형 처리
    991px / 576px 기반으로 반응형 처리함.

--- src/component/Event.js
+++ src/component/Event.js
@@ -5,29 +5,31 @@ function Event(props) {
   return (
     <section id={props.id} className={`${e.section} py-5`}>
         <ul className={`d-lg-flex`}>
-            <li>
-              <div  className={`event0 ${e.eventlist}`}>
-                <a href=""></a>
+            <li className='lgE'>
+                <div>
+                    <div  className={`event0 ${e.eventlist}`}>
+                        <a href=""></a>
+                    </div>
+                </div>
+            </li>
+            <li className='smE'>
+              <div className={`d-sm-flex align-items-center`}>
+                  <div i className={`event1 ${e.eventlist} ${e.smbox}`}>
+                      <a href=""></a>
+                  </div>
+                  <div className={`${e.textbox}`}>
+                      <p className={`mb-0`}>캠핑에 미치다 X 온가담</p>
+                      <span>최대 30%세일 오픈!!</span>
+                      <a href="">바로가기 <img src="/img/arrow.png" alt="arrow" /></a>
+                  </div>
+              </div>
+              <div className={`d-sm-flex align-items-center`}>
+                  <div className={`${e.textbox}`}>설명입니다.</div>
+                  <div i className={`event2 ${e.eventlist} ${e.smbox}`}>
+                      <a href=""></a>
+                  </div>
               </div>
             </li>
-            <div>
-              <li className={`d-lg-flex align-items-center`}>
-                <div i className={`event1 ${e.eventlist} ${e.smbox}`}>
-                  <a href=""></a>
-                </div>
-                <div className={`${e.textbox}`}>
-                  <p className={`mb-0`}>캠핑에 미치다 X 온가담</p>
-                  <span>최대 30%세일 오픈!!</span>
-                  <a href="">바로가기 <img src="/img/arrow.png" alt="arrow" /></a>
-                </div>
-              </li>
-              <li className={`d-lg-flex align-items-center`}>
-                <div className={`${e.textbox}`}>설명입니다.</div>
-                <div i className={`event2 ${e.eventlist} ${e.smbox}`}>
-                  <a href=""></a>
-                </div>
-              </li>
-            </div>
         </ul>
     </section>
   )
