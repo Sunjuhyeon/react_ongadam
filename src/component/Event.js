2677595c056ec5b0f7af3d444554cd359450347f
dongsam-E <dong2dong32@gmail.com>
2023-08-10 18:08:04 +0900

    2차 반응형 처리
    1. 576px 이하 시 사진 화면에 95vw로 꽉 차고
    모바일용 멘트 d-block처리 완료

--- src/component/Event.js
+++ src/component/Event.js
@@ -7,28 +7,47 @@ function Event(props) {
         <ul className={`d-lg-flex`}>
             <li className='lgE'>
                 <div>
-                    <div  className={`event0 ${e.eventlist}`}>
+                    <div className={`eventimg event0 ${e.eventlist}`}>
                         <a href=""></a>
+                    </div>
+                    <div className={`${e.smtext} d-none`}>
+                        <p>남양주 인기카페 <em>라온숨 X 온가담</em> 콜라보!!</p>
+                        <span>무료체험&무료커피 맛보러 GoGo~</span>
                     </div>
                 </div>
             </li>
-            <li className='smE'>
-              <div className={`d-sm-flex align-items-center`}>
-                  <div i className={`event1 ${e.eventlist} ${e.smbox}`}>
-                      <a href=""></a>
-                  </div>
-                  <div className={`${e.textbox}`}>
-                      <p className={`mb-0`}>캠핑에 미치다 X 온가담</p>
-                      <span>최대 30%세일 오픈!!</span>
-                      <a href="">바로가기 <img src="/img/arrow.png" alt="arrow" /></a>
-                  </div>
-              </div>
-              <div className={`d-sm-flex align-items-center`}>
-                  <div className={`${e.textbox}`}>설명입니다.</div>
-                  <div i className={`event2 ${e.eventlist} ${e.smbox}`}>
-                      <a href=""></a>
-                  </div>
-              </div>
+            <li className={`${e.smE}`}>
+                    {
+                        props.info.contentsdb.event.smE.map((v, i) => {
+                            return(
+                                <div className={`d-sm-flex align-items-center`}>
+                                    <div i className={`${v.imgcls} ${e.eventlist} ${e.smbox}`}>
+                                        <a href=""></a>
+                                    </div>
+                                    <div className={`${v.textcls} ${e.textbox}`}>
+                                        <p className={`mb-0`}>{v.title}</p>
+                                        <span>{v.subtitle}</span>
+                                        <a href="">바로가기<img src="/img/arrow.png" alt="arrow" /></a>
+                                    </div>
+                                    <div className={`${e.smtext} d-none`}>
+                                        <p>
+                                            {
+                                                v.smtext.split("<em>").map((em, idx) => {
+                                                    if (idx % 2 === 0){
+                                                        return em
+                                                    } else{
+                                                        return <em key={idx}>{em}</em>
+                                                    }
+                                                    
+                                                })
+                                            }
+                                        </p>
+                                        <span>{v.smtextsub}</span>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
             </li>
         </ul>
     </section>
