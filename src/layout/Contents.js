41b365dbd81d405b6c8a6e4c4a60daf841b8d18d
dongsam-E <dong2dong32@gmail.com>
2023-08-14 18:07:23 +0900

    리뷰 컴포넌트 추가

--- src/layout/Contents.js
+++ src/layout/Contents.js
@@ -11,9 +11,9 @@ function Contents(props) {
         <div id={props.id}>
             <Slide id="slide" info={props.info}></Slide>
             <EventBanner id="MEventBanner" info={props.info}></EventBanner>
+            <Review id="review" info={props.info}></Review>
             <Event id="event" info={props.info}></Event>
             <About id="about" info={props.info}></About>
-            <Review id="review" info={props.info}></Review>
             <Button id="button"></Button>
         </div>
     );
