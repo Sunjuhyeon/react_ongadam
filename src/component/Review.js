51cb84e51ee54485598e40f4fe6a4d27ed2878fd
dongsam-E <dong2dong32@gmail.com>
2023-08-16 18:02:07 +0900

    세로스와이퍼 구현

--- src/component/Review.js
+++ src/component/Review.js
@@ -1,93 +1,88 @@
-import React from 'react'
+import React, {useState} from 'react'
 import re from '../scss/review.module.css'
-import { Controller, Scene } from 'react-scrollmagic';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination, FreeMode, Mousewheel } from 'swiper/modules';
 
 
 function Review(props) {
-  return (
-    <section id={props.id} className={`${re.section} d-flex`}>
-      <div className={`${re.merit}`}>
-        <p className={`mb-0`}>고객 구매 만족도</p>
-        <div className={`d-flex`}>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-        </div>
-        <div>
-          <div className={`d-flex`}>
-            <i class="bi bi-check2-square"></i>
-            <p>텀블러 크기의 미니 사이즈</p>
-          </div>
-          <div className={`d-flex`}>
-            <i class="bi bi-check2-square"></i>
-            <p>저렴한 유지비용</p>
-          </div>
-          <div className={`d-flex`}>
-            <i class="bi bi-check2-square"></i>
-            <p>옷 손상 최소화</p>
-          </div>
-          <div className={`d-flex`}>
-            <i class="bi bi-check2-square"></i>
-            <p>신발 건조</p>
-          </div>
-          <div className={`d-flex`}>
-            <i class="bi bi-check2-square"></i>
-            <p>UV살균 램프</p>
-          </div>
-          <div className={`d-flex`}>
-            <i class="bi bi-check2-square"></i>
-            <p>제습, 탈취</p>
-          </div>
-          <div className={`d-flex`}>
-            <i class="bi bi-check2-square"></i>
-            <p>저소음</p>
-          </div>
-        </div>
-      </div>
-      <div className={`${re.reviewsection}`}>
-        <div className={`${re.title}`}>
-          <p>real review</p>
-          <span>써본 사람들의 진짜 후기</span>
-        </div>
-        <div className={`${re.reviewwrap}`}>
-          {
-            props.info.contentsdb.review.map((v, i) => {
-              return(
-                <div key={i} className={`${re.reviewbox} box${i}`}>
-                  <div>
-                    <div className={`${re.hd}`}>
-                      <div className={`d-flex`}>
+    return (
+        <section id={props.id} className={`d-flex ${re.section} `}>
+            <div className={`${re.merit}`}>
+                <div className={`${re.mTitle}`}>
+                    <p className={`mb-0`}>고객 구매 만족도</p>
+                    <div className={`d-flex`}>
                         <i class="bi bi-star-fill"></i>
                         <i class="bi bi-star-fill"></i>
                         <i class="bi bi-star-fill"></i>
                         <i class="bi bi-star-fill"></i>
                         <i class="bi bi-star-fill"></i>
-                      </div>
-                      <p>{v.name}</p>
                     </div>
-                    <div className={`${re.body}`}>
-                      <img src={v.src} alt="review0" />
-                      <div>
-                        {v.re.split("<br>").map((br, idx)=> {
-                          return(
-                            <p key={idx} className={`${re.bodytxt} re${idx}`}>
-                              {br}
-                            </p>
-                          )
-                        })}
-                      </div>
-                    </div>
-                  </div>
                 </div>
-              )
-            })
-          }
-        </div>
-      </div>
-    </section>
-  )
+                <div className={`${re.filterbox}`}>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div className={`${re.review}`}>
+                <div className={`${re.rTitle}`}>
+                    <p>real review</p>
+                    <span>써본 사람들의 진짜 리뷰</span>
+                </div>
+                <Swiper
+                    direction={'vertical'}
+                    pagination={{
+                        type: 'progressbar',
+                    }}
+                    slidesPerView={'auto'}
+                    freeMode={true}
+                    mousewheel={true}
+                    modules={[Pagination, FreeMode, Mousewheel]}
+                    className="mySwiper h-75"
+                >
+                    {
+                        props.info.contentsdb.review.map((v, i) => {
+                            return(
+                                <SwiperSlide className={`${re.swiperslide}`}>
+                                    <div className={`${re.reviewbox}`}>
+                                        <div className={`${re.hd}`}>
+                                            <div>
+                                                <i class="bi bi-star-fill"></i>
+                                                <i class="bi bi-star-fill"></i>
+                                                <i class="bi bi-star-fill"></i>
+                                                <i class="bi bi-star-fill"></i>
+                                                <i class="bi bi-star-fill"></i>
+                                            </div>
+                                            <div>
+                                                <span>{v.name}</span>
+                                            </div>
+                                        </div>
+                                        <div className={`${re.body} d-flex`}>
+                                            <img src={v.src} alt="review" />
+                                            <div className={`${re.txt}`}>
+                                                {
+                                                    v.re.split("<br>").map((val, idx) => {
+                                                        return(
+                                                            <p className={`re${idx}`}>{val}</p>
+                                                        )
+                                                    })
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            ) 
+                        })
+
+                    }
+
+                </Swiper>
+            </div>
+        </section>
+    )
 }
 
 export default Review
