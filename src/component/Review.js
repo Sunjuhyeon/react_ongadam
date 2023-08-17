9a74ff469b0f86fb0ee7997ecb8999b5786b9a42
dongsam-E <dong2dong32@gmail.com>
2023-08-17 13:16:37 +0900

    리뷰섹션 디테일 디자인 조정 1. 전체적으로 크기 작게 해서 한눈에 보이게

--- src/component/Review.js
+++ src/component/Review.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import re from '../scss/review.module.css'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -9,78 +9,89 @@ import { Pagination, FreeMode, Mousewheel } from 'swiper/modules';
 
 function Review(props) {
     return (
-        <section id={props.id} className={`d-flex ${re.section} `}>
-            <div className={`${re.merit}`}>
-                <div className={`${re.mTitle}`}>
-                    <p className={`mb-0`}>고객 구매 만족도</p>
-                    <div className={`d-flex`}>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
+        <section id={props.id} className={` ${re.section}`}>
+            <div className='px-lg-0 d-flex flex-column flex-lg-row justify-content-lg-between'>
+                <div className={`${re.merit} col-lg-5`}>
+                    <div className={`${re.mTitle}`}>
+                        <p className={`mb-0`}>고객 구매 만족도</p>
+                        <div className={`d-flex`}>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                        </div>
                     </div>
-                </div>
-                <div className={`${re.filterbox}`}>
-                    <div>
-                        
-                    </div>
-                </div>
-            </div>
-            <div className={`${re.review}`}>
-                <div className={`${re.rTitle}`}>
-                    <p>real review</p>
-                    <span>써본 사람들의 진짜 리뷰</span>
-                </div>
-                <Swiper
-                    direction={'vertical'}
-                    pagination={{
-                        type: 'progressbar',
-                    }}
-                    slidesPerView={'auto'}
-                    freeMode={true}
-                    mousewheel={true}
-                    modules={[Pagination, FreeMode, Mousewheel]}
-                    className="mySwiper h-75"
-                >
-                    {
-                        props.info.contentsdb.review.map((v, i) => {
-                            return(
-                                <SwiperSlide className={`${re.swiperslide}`}>
-                                    <div className={`${re.reviewbox}`}>
-                                        <div className={`${re.hd}`}>
-                                            <div>
-                                                <i class="bi bi-star-fill"></i>
-                                                <i class="bi bi-star-fill"></i>
-                                                <i class="bi bi-star-fill"></i>
-                                                <i class="bi bi-star-fill"></i>
-                                                <i class="bi bi-star-fill"></i>
-                                            </div>
-                                            <div>
-                                                <span>{v.name}</span>
-                                            </div>
-                                        </div>
-                                        <div className={`${re.body} d-flex`}>
-                                            <img src={v.src} alt="review" />
-                                            <div className={`${re.txt}`}>
-                                                {
-                                                    v.re.split("<br>").map((val, idx) => {
-                                                        return(
-                                                            <p className={`re${idx}`}>{val}</p>
-                                                        )
-                                                    })
-                                                }
-                                            </div>
-                                        </div>
+                    <div className={`${re.meritlist}`}>
+                        {
+                            props.info.contentsdb.review.checkbox.map((v, i) => {
+                                return (
+                                    <div>
+                                        <img src={v.src} alt="checkbox" />
+                                        <span>{v.title}</span>
                                     </div>
-                                </SwiperSlide>
-                            ) 
-                        })
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div className={`${re.review} col`}>
+                    <div className={`${re.rTitle}`}>
+                        <p>real review</p>
+                        <span>써본 사람들의 진짜 리뷰</span>
+                    </div>
+                    <Swiper
+                        direction={'vertical'}
+                        pagination={{
+                            type: 'progressbar',
+                        }}
+                        slidesPerView={'auto'}
+                        freeMode={true}
+                        mousewheel={true}
+                        modules={[Pagination, FreeMode, Mousewheel]}
+                        className={`mySwiper ${re.swiper}`}
+                    >
+                        {
+                            props.info.contentsdb.review.reviewbox.map((v, i) => {
+                                return (
+                                    <SwiperSlide className={`${re.swiperslide}`}>
+                                        <div className={`${re.reviewbox}`}>
+                                            <div className={`${re.hd}`}>
+                                                <div>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                </div>
+                                                <div>
+                                                    <span>{v.name}</span>
+                                                </div>
+                                            </div>
+                                            <div className={`${re.body} d-flex`}>
+                                                <img src={v.src} alt="review" />
+                                                <div className={`${re.txt}`}>
+                                                    {
+                                                        v.re.split("<br>").map((val, idx) => {
+                                                            return (
+                                                                <p className={`re${idx}`}>{val}</p>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                )
+                            })
 
-                    }
+                        }
 
-                </Swiper>
+                    </Swiper>
+                </div>
+
             </div>
+
         </section>
     )
 }
