c7c124e84f63d12bb894772974fce5791c693f3f
dongsam-E <dong2dong32@gmail.com>
2023-08-17 18:09:19 +0900

    aos import

--- src/layout/Contents.js
+++ src/layout/Contents.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import 'animate.css';
 
 import Slide from '../component/Slide'
 import Event from '../component/Event'
@@ -8,6 +11,9 @@ import EventBanner from '../component/EventBanner'
 import Button from '../component/Button'
 
 function Contents(props) {
+    useEffect(() => {
+        AOS.init();
+    })
     return (
         <div id={props.id}>
             <Slide id="slide" info={props.info}></Slide>
