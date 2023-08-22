4e0db0371d2263842652f6e105c85302ee395a83
dongsam-E <dong2dong32@gmail.com>
2023-08-22 18:12:17 +0900

    폼 기본 레이아웃 지정

--- src/component/Form.js
+++ src/component/Form.js
@@ -8,32 +8,26 @@ function Form(props) {
     console.log(watch("example")); 
 
   return (
-    <section id={props.id} className={`py-5`}>
-        <form onSubmit={handleSubmit(onSubmit)} className={`${form.formBox}`}>
-            <label htmlFor="name">이름</label>
-            <input 
-                id="name" 
-                type="text" 
-                placeholder="홍길동"
-                {...register("name")}
-            />
-            <label htmlFor="number">전화번호</label>
-            <input 
-                id="number" 
-                type="tel" 
-                placeholder="01000000000"
-                {...register("number")}
-            />
-            <label htmlFor="date">신청 날짜</label>
-            <input 
-                id="date" 
+    <section id={props.id} className={`${form.sectionbg} py-5`}>
+        <form onSubmit={handleSubmit(onSubmit)} className={`${form.formBox} d-flex`}>
+            <div>
+                <div>
+                    <input
+                    type="text"
+                    placeholder="이름"
+                    />
+                    <input
+                    type="text"
+                    placeholder="연락처"
+                    />
+                </div>
+                <input
                 type="date"
-                min="2023-09-01"
-                max="2023-09-30"
-                data-placeholder="날짜선택"
-                required
-             />
-            <button type="submit">신청하기</button>
+                placeholder="방문날짜"
+                className={`w-100`}
+                />
+            </div>
+            <input type="submit" value="신청"/>
         </form>
     </section>
   )
