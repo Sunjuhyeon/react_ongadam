b7b1bf12bdfa48e81915497315c439472b46334b
dongsam-E <dong2dong32@gmail.com>
2023-08-25 16:41:43 +0900

    1. 이벤트섹션 디자인변경

--- src/component/Event.js
+++ src/component/Event.js
@@ -5,29 +5,19 @@ function Event(props) {
   return (
     <section id={props.id} className={`${e.section}`}>
         <div className={`${e.titlename}`}>
-            <p>ongadam <em>EVENT</em></p>
+            <p>진행중인 이벤트</p>
         </div>
-        <ul className={`d-lg-flex`}>
-            <li className='lgE'>
-                <div>
-                    <div className={`eventimg event0 ${e.eventlist}`}>
-                        <a href=""></a>
-                    </div>
-                    <div className={`${e.smtext} d-none`}>
-                        <p>남양주 인기카페 <em>라온숨 X 온가담</em> 콜라보!!</p>
-                        <span>무료체험&무료커피 맛보러 GoGo~</span>
-                    </div>
-                </div>
-            </li>
-            <li className={`${e.smE}`}>
-                    {
-                        props.info.contentsdb.event.smE.map((v, i) => {
-                            return(
-                                <div className={`d-sm-flex align-items-center`}>
-                                    <div i className={`${v.imgcls} ${e.eventlist} ${e.smbox}`}>
-                                        <a href=""></a>
-                                    </div>
-                                    <div className={`${v.textcls} ${e.textbox}`}>
+        <ul className={`container-lg d-flex flex-column flex-md-row justify-content-center align-items-center`}>
+            {
+                props.info.contentsdb.event.list.map((v, i) => {
+                    return(
+                        <li className={`${e.eventlist} d-sm-flex align-itmes-center`}>
+                            <div className='eventbigbox'>
+                                <div className={`${v.imgcls}`}>
+                                    <a href=""></a>
+                                </div>
+                                <div className={`${v.textcls} ${e.textbox}`}>
+                                    <div className={`${e.textbigbox}`}>
                                         {v.title.split("<br>").map((br, i) => {
                                             const emSplit = br.split("<em>");
                                             return (
@@ -44,33 +34,18 @@ function Event(props) {
                                         })}
                                         {
                                             v.subtitle.split("<br>").map((br, idx) => {
-                                                return(
+                                                return (
                                                     <p className={`subtitle${idx}`}>{br}</p>
                                                 )
                                             })
                                         }
-                                        <a href="">바로가기<img src="/img/arrow.png" alt="arrow" /></a>
-                                    </div>
-                                    <div className={`${e.smtext} d-none`}>
-                                        <p>
-                                            {
-                                                v.smtext.split("<em>").map((em, idx) => {
-                                                    if (idx % 2 === 0){
-                                                        return em
-                                                    } else{
-                                                        return <em key={idx}>{em}</em>
-                                                    }
-                                                    
-                                                })
-                                            }
-                                        </p>
-                                        <span>{v.smtextsub}</span>
                                     </div>
                                 </div>
-                            )
-                        })
-                    }
-            </li>
+                            </div>
+                        </li>
+                    )
+                })
+            }
         </ul>
     </section>
   )
