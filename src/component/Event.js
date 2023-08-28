32ef255d46197f6a11fbbc41183f10adf3cd9603
dongsam-E <dong2dong32@gmail.com>
2023-08-28 18:08:45 +0900

    991px 이상 시 스와이퍼 옵션 바꾸기 구현중

--- src/component/Event.js
+++ src/component/Event.js
@@ -4,9 +4,56 @@ import e from '../scss/event.module.css'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
+// import { Navigation } from 'swiper/modules';
 
 function Event(props) {
+
+    const [eventSwiper, setEventSwiper] = useState(null);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        function initSwiper(){
+            if(eventSwiper){
+                eventSwiper.destroy();
+            };
+
+            if(screenWidth > 991){
+                setEventSwiper(
+                    new Swiper('.mySwiper', {
+                        slidesPerView: 3,
+                        spaceBetween: 10,
+                    })
+                );
+            } else{
+                setEventSwiper(
+                    new Swiper('.mySwiper', {
+                        slidesPerView: 1.5,
+                        spaceBetween: 10,
+                        loop : true,
+                        centeredSlides: true,
+                    })
+                );
+            }
+        }
+
+        initSwiper();
+
+        window.addEventListener('resize', () => {
+            setScreenWidth(window.innerWidth);
+        });
+
+        return() => {
+            if(eventSwiper){
+                eventSwiper.destroy();
+            }
+            window.removeEventListener('resize', () => {
+                setScreenWidth(window.innerWidth);
+            });
+        };
+    }, [screenWidth, eventSwiper]);
+
+
+    
     
   return (
     <section id={props.id} className={`${e.section}`}>
@@ -14,22 +61,22 @@ function Event(props) {
             <p>진행중인 이벤트</p>
         </div>
         <Swiper 
-        navigation={true}
-        slidesPerView={1.5}
-        spaceBetween={10}
-        centeredSlides={true}
-        modules={[Navigation]}
-        breakpoints={{
-            768: {
-                slidesPerView: 3,
-                centeredSlides:false,
-            }
-        }}
+        // navigation={true}
+        // slidesPerView={1.5}
+        // spaceBetween={10}
+        // centeredSlides={true}
+        // modules={[Navigation]}
+        // breakpoints={{
+        //     768: {
+        //         slidesPerView: 3,
+        //         centeredSlides:false,
+        //     }
+        // }}
         className={`mySwiper ${e.eventbox} container-lg d-flex flex-column flex-md-row justify-content-md-between justify-content-center align-items-center`}>
             {
                 props.info.contentsdb.event.list.map((v, i) => {
                     return(
-                        <SwiperSlide className={`col-md-4`}>
+                        <SwiperSlide className={`col-md-4`} key={i}>
                             <li className={`${e.eventlist} d-sm-flex align-itmes-center`}>
                                 <div className='eventbigbox'>
                                     <div className={`${v.imgcls}`}>
